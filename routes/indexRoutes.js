a9313881c11451695280c2b0897b120fd4cf6683
Jenya <jeskov95@gmail.com>
2022-02-12 06:55:38 +0200

    rename service router to control router

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 // import reminderController from "../controllers/reminderController";
 import reminderRoutes from "./reminderRoutes.js";
-import serviceRoutes from "./serviceRoutes.js";
+import controlRoutes from "./coontolRoutes.js";
 
 const router = express.Router();
 
 router.use("/reminders", reminderRoutes);
-router.use("/service", serviceRoutes);
+router.use("/control", controlRoutes);
 
 router.get('/', (req, res) => {
     res.send('deployed');
