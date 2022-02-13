0d679db38d7a557244f67f87058500864af48b21
Jenya <jeskov95@gmail.com>
2022-02-13 08:20:34 +0200

    further add delete realization

--- models/reminderService.js
+++ models/reminderService.js
@@ -5,17 +5,15 @@ class ReminderService {
     async getReminder(data) {
         if (data.todays.toLowerCase() === "todays") {
             return Reminder.find({"messenger user id": data["messenger user id"]});  //TODO
-        }
-        else if (data.todays.toLowerCase() === "all") {
+        } else if (data.todays.toLowerCase() === "all") {
             return Reminder.find({"messenger user id": data["messenger user id"]});
-        }
-        else return "Invalid input"
+        } else return "Invalid input"
 
     }
 
     async createReminder(data) {
         const reminder = await new Reminder({...data});
-        reminder.userReminderId = getNewId();                    // TODO
+        // reminder.userReminderId = getNewId();                    // TODO
 
         await reminder.save();
 
@@ -24,11 +22,17 @@ class ReminderService {
     }
 
     async deleteReminder(data) {
-        // Reminder.findOneAndRemove({...data});
-        const reminder = await Reminder.findOneAndDelete({...data});
 
-        console.log(reminder);
-        return reminder;
+        if (data.amount.toLowerCase() === ("one" || 1)) {
+            return Reminder.findOneAndDelete({
+                userReminderId: data.userReminderId,
+                "messenger user id": data["messenger user id"]
+            });
+        }
+        else if (data.amount.toLowerCase() === "all") {
+            return Reminder.deleteMany({"messenger user id": data["messenger user id"]});
+        }
+        else return "Invalid input"
 
     }
 
