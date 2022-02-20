528b0d8bdea39f481d9ad37f0f536cfc30d4867d
Jenya <jeskov95@gmail.com>
2022-02-20 11:18:59 +0200

    create utils class, and move methods from service

--- models/reminderService.js
+++ models/reminderService.js
@@ -1,10 +1,6 @@
 import reminderDB from "./mongoDbWithMongoose.js";
 import DateAndTime from "date-and-time"
-
-const dateAndTimePatterns = ['DD.MM.YYYY HH.mm', 'D.MM.YYYY HH.mm', 'DD.MM.YYYY H.mm',
-    'D.MM.YYYY H.mm', 'DD.MM.YY HH.mm', 'D.MM.YY HH.mm', 'DD.MM.YY H.mm', 'D.MM.YY H.mm',
-    'DD.M.YYYY HH.mm', 'D.M.YYYY HH.mm', 'DD.M.YYYY H.mm', 'D.M.YYYY H.mm', 'DD.M.YY HH.mm',
-    'D.M.YY HH.mm', 'DD.M.YY H.mm', 'D.M.YY H.mm'];
+import Utils from "./reminderUtils.js";
 
 class ReminderService {
 
@@ -17,34 +13,11 @@ class ReminderService {
 
     }
 
-    async getReminderTime(date, time, offset) {
-        for (const pattern of dateAndTimePatterns) {
-            let timeString = `${date}T${time} ${this.formatOffset(offset)}`;
-
-            if (DateAndTime.isValid(timeString, `${pattern} Z`)) {
-                return DateAndTime.parse(timeString, `${pattern} Z`);
-            }
-        }
-    }
-
     async createReminder(data) {
-        data.userReminderId = await this.getNewId(data["messenger user id"]);
-        data.time = await this.getReminderTime(data.dateInput, data.timeInput, data.timezone);
+        data.userReminderId = await Utils.getNewId(data["messenger user id"]);
+        data.time = await Utils.getReminderTime(data.dateInput, data.timeInput, data.timezone);
 
         return await reminderDB.createNew(data);
-    }
-
-    async getNewId(messengerId) {
-        const reminders = await this.getReminders({"messenger user id": messengerId, amount: "all"});
-        const existingIDs = reminders.map((reminder) => reminder.userReminderId);
-
-        let id = 1;
-
-        while (existingIDs.includes(id)) {
-            id += 1;
-        }
-
-        return id;
     }
 
     async deleteReminder(data) {
@@ -62,34 +35,6 @@ class ReminderService {
 
     }
 
-    formatOffset(offset) {
-        const separators = [".", ":", "/"];
-
-        for (const separator of separators) {
-            if (offset.includes(separator)) {
-                offset = offset.split(separator).join("");
-                break;
-            }
-        }
-
-        let sign;
-        if (offset >= 0) {
-            sign = "+";
-        } else {
-            sign = "-";
-            offset = offset.slice(1);
-        }
-
-        if (offset.length === 1) {
-            return `${sign}0${offset}00`
-        } else if (offset.length === 2) {
-            return `${sign}${offset}00`
-        } else if (offset.length === 3) {
-            return `${sign}0${offset}`
-        } else if (offset.length === 4) {
-            return `${sign}${offset}`
-        }
-    }
 }
 
 export default new ReminderService();
---
+++ models/reminderUtils.js
@@ -0,0 +1,63 @@
+import DateAndTime from "date-and-time";
+import reminderService from "./reminderService.js";
+
+class ReminderUtils {
+    dateAndTimePatterns = ['DD.MM.YYYY HH.mm', 'D.MM.YYYY HH.mm', 'DD.MM.YYYY H.mm',
+        'D.MM.YYYY H.mm', 'DD.MM.YY HH.mm', 'D.MM.YY HH.mm', 'DD.MM.YY H.mm', 'D.MM.YY H.mm',
+        'DD.M.YYYY HH.mm', 'D.M.YYYY HH.mm', 'DD.M.YYYY H.mm', 'D.M.YYYY H.mm', 'DD.M.YY HH.mm',
+        'D.M.YY HH.mm', 'DD.M.YY H.mm', 'D.M.YY H.mm'];
+
+    async getReminderTime(date, time, offset) {
+        for (const pattern of this.dateAndTimePatterns) {
+            let timeString = `${date}T${time} ${this.formatOffset(offset)}`;
+
+            if (DateAndTime.isValid(timeString, `${pattern} Z`)) {
+                return DateAndTime.parse(timeString, `${pattern} Z`);
+            }
+        }
+    }
+
+    formatOffset(offset) {
+        const separators = [".", ":", "/"];
+
+        for (const separator of separators) {
+            if (offset.includes(separator)) {
+                offset = offset.split(separator).join("");
+                break;
+            }
+        }
+
+        let sign;
+        if (offset >= 0) {
+            sign = "+";
+        } else {
+            sign = "-";
+            offset = offset.slice(1);
+        }
+
+        if (offset.length === 1) {
+            return `${sign}0${offset}00`
+        } else if (offset.length === 2) {
+            return `${sign}${offset}00`
+        } else if (offset.length === 3) {
+            return `${sign}0${offset}`
+        } else if (offset.length === 4) {
+            return `${sign}${offset}`
+        }
+    }
+
+    async getNewId(messengerId) {
+        const reminders = await reminderService.getReminders({"messenger user id": messengerId, amount: "all"});
+        const existingIDs = reminders.map((reminder) => reminder.userReminderId);
+
+        let id = 1;
+
+        while (existingIDs.includes(id)) {
+            id += 1;
+        }
+
+        return id;
+    }
+}
+
+export default new ReminderUtils();
