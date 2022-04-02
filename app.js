168fdab05e7b21908f5b6ac396cf9f70cf65df3e
Jenya <jeskov95@gmail.com>
2022-04-02 06:53:00 +0300

    use error handling middleware in app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import bodyParser from "body-parser";
 import {MongoDB} from "./config/mongoDB.js";
 import router from "./routes/indexRoutes.js";
 
+import errorHandler from "./Errors/errorHandler.js";
+
+import controlService from "./models/controlService.js";
+
 const port = process.env.PORT || 58588;
 const app = express();
 const mongoConnection = new MongoDB("mongo connection 1", process.env.MONGO_URI)
@@ -11,6 +15,8 @@ const mongoConnection = new MongoDB("mongo connection 1", process.env.MONGO_URI)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(router);
+
+app.use(errorHandler);
 
 async function start() {
     app.listen(port, () => console.log('Server is running'));
