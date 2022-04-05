4ad8b4f51a9f00812ed81bf9c8d4fa85114f1ad2
Jenya <jeskov95@gmail.com>
2022-04-05 06:30:16 +0300

    add binding of controller context in reminder router

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -3,13 +3,12 @@ import reminderController from "../controllers/reminderController.js";
 
 const router = express.Router();
 
-router.post('/getRems', reminderController.getReminders);
+router.post('/getRems', reminderController.getReminders.bind(reminderController));
 
 router.post('/addRem', reminderController.createReminder.bind(reminderController));
-// router.post('/addRem', reminderController.createReminder);
 
-router.post('/delete', reminderController.deleteReminder);
+router.post('/delete', reminderController.deleteReminder.bind(reminderController));
 
-router.post('/acceptOrSnooze', reminderController.acceptOrSnoozeReminder);
+router.post('/acceptOrSnooze', reminderController.acceptOrSnoozeReminder.bind(reminderController));
 
 export default router;
