c639820b8fed1b1a2d28e6a73a7016fdf4be40a0
Jenya <jeskov95@gmail.com>
2022-05-17 05:39:58 +0300

    hooks module refactor and clean up

--- config/hooks.js
+++ config/hooks.js
@@ -1,23 +1,10 @@
 export function applyHooks(db, connection) {
-    db.on('connected', function () {
+    db.on('connected', () => {
         console.log(`Connected to ${connection.name}`);
         connection.failsCount = 0;
     });
 
-    db.on("error", async () => {
-        console.log("failed");
-        connection.failsCount += 1;
-        console.log(connection.failsCount)
-
-        if (connection.failsCount <= 3) {
-            console.log(`reconnecting.............`);
-            await connection.connect()
-        } else {
-            console.log("fuck of!!!!!!!!!!!!!!!!!1")
-        }
-    });
-
-    process.on('SIGINT', function() {
+    process.on('SIGINT', () => {
         db.close(function () {
             console.log(`${connection.name} disconnected through app termination`);
             process.exit(0);
