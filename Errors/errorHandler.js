ab910e5fb3601b76f2ee17f6d438b52ca63ee14c
Jenya <jeskov95@gmail.com>
2022-06-02 20:12:26 +0300

    get rid of Api errors loging

--- Errors/errorHandler.js
+++ Errors/errorHandler.js
@@ -1,12 +1,12 @@
 import ApiError from "./apiError.js";
 
 export default function handle(err, req, res, next) {
-    console.error(err);
 
     if (err instanceof ApiError) {
-    res.status(err.status);
+        res.status(err.status);
         return res.send(err.message);
     } else {
+        console.error(err);
         return res.status(500).send("Something went wrong, please try again.");
     }
 }
