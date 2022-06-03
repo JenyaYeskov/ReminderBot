8d33b6c9468362a99753611e14f38b80e3be27b7
Jenya <jeskov95@gmail.com>
2022-06-03 04:39:23 +0300

    working on reminder view

--- Errors/errorHandler.js
+++ Errors/errorHandler.js
@@ -1,12 +1,13 @@
 import ApiError from "./apiError.js";
+import reminderView from "../view/reminderView.js";
 
 export default function handle(err, req, res, next) {
 
     if (err instanceof ApiError) {
         res.status(err.status);
-        return res.send(err.message);
+        return res.send(reminderView.showResponse(err.message));
     } else {
         console.error(err);
-        return res.status(500).send("Something went wrong, please try again.");
+        return res.status(500).send(reminderView.showResponse("Something went wrong, please try again."));
     }
 }
