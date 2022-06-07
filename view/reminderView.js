639dd7f15df0947b45831404c3d6c2c150e68504
Jenya <jeskov95@gmail.com>
2022-06-07 21:50:53 +0300

    code cleanup

--- view/reminderView.js
+++ view/reminderView.js
@@ -2,15 +2,12 @@ class reminderView {
     showResponse(data) {
         if (Array.isArray(data)) {
             let response = [];
-            let re = [];
 
             for (let i = 0; i < data.length; i++) {
                 response.push({"text": `${data[i].userReminderId} : ${data[i].event} : ${data[i].time}`})
             }
-            re.push(response)
-            re.push(response)
 
-            return re
+            return response
         } else if (typeof data === "string") {
             return [{"text": data}]
         } else if (typeof data === "object") {
