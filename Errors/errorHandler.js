8147bbc808b5ae2b2791a5acc4634e92d4a13ebe
Jenya <jeskov95@gmail.com>
2022-06-07 20:51:19 +0300

    add status code correction for chatfuel

--- Errors/errorHandler.js
+++ Errors/errorHandler.js
@@ -4,10 +4,22 @@ import reminderView from "../view/reminderView.js";
 export default function handle(err, req, res, next) {
 
     if (err instanceof ApiError) {
+        console.log(req)
         res.status(err.status);
+        adjustStatus(req, res);
+
         return res.send(reminderView.showResponse(err.message));
     } else {
         console.error(err);
-        return res.status(500).send(reminderView.showResponse("Something went wrong, please try again."));
+        res.status(500);
+        adjustStatus(req, res);
+
+        return res.send(reminderView.showResponse("Something went wrong, please try again."));
+    }
+}
+
+function adjustStatus(req, res) {
+    if (req.headers["user-agent"] === "chatfuel"){
+        res.status(200);
     }
 }
