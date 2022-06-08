72cd7a1b5a7f5b73cd6432d3f36edb68590b8fc1
Jenya <jeskov95@gmail.com>
2022-06-08 23:57:24 +0300

    working firing reminder request

--- models/reminderService.js
+++ models/reminderService.js
@@ -60,11 +60,11 @@ class ReminderService {
 
             for (const reminder of reminders) {
                 if (reminder.time <= new Date()) {
-                    console.log(reminder._id)
+                    console.log(reminder._id.toString())
                     // await this.acceptOrSnoozeReminder(reminder)
                     return await axios({
                         method: 'post',
-                        url: `https://api.chatfuel.com/bots/61e8d5515f917b145dff3bfe/users/${reminder["messenger user id"]}/send?chatfuel_token=FLVgqoIjXiykg6Ameirt8c5sg7nDpuSfoefL24E12rtOe6I1wxc2SrwN3d5Bn39G&chatfuel_flow_name=Flow2&event=Time to ${reminder.event}&dbReminderId=${reminder._id}`,
+                        url: `https://api.chatfuel.com/bots/61e8d5515f917b145dff3bfe/users/${reminder["messenger user id"]}/send?chatfuel_token=FLVgqoIjXiykg6Ameirt8c5sg7nDpuSfoefL24E12rtOe6I1wxc2SrwN3d5Bn39G&chatfuel_flow_name=Flow2&event=Time to ${reminder.event}&dbReminderId=${reminder._id.toString()}`,
                         headers: { 'Content-Type': 'application/json' }
                     });
 
