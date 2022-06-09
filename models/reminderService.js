dd71e54ac9af9c59f619757607f909f708389f46
Jenya <jeskov95@gmail.com>
2022-06-09 03:24:52 +0300

    add response data check to reminderController.js

--- models/reminderService.js
+++ models/reminderService.js
@@ -99,7 +99,7 @@ class ReminderService {
 
             let rem = await reminderDB.find({id: dbReminderId});
 
-            this.activateReminder(rem[0], "q");
+            this.activateReminder(rem[0], "");
         }
     }
 
