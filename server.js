5a065884ae921f7a6d4b63649e4bf92afb05cd68
Jenya <jeskov95@gmail.com>
2022-06-21 06:10:37 +0300

    slight code formatting in the server.js

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ import app from "./app.js";
 import {MongooseConnection} from "./config/mongooseConnection.js";
 
 const port = process.env.PORT || 58588;
-
 const mongoConnection = new MongooseConnection("mongo connection 1", process.env.MONGO_URI);
 
-app.listen(port, () => console.log('Server is running'));
+app.listen(port, () => console.log("Server is running"));
 
 try {
     await mongoConnection.connect();
