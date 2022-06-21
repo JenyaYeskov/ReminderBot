7de2e57d6f7682dfa204a36f724d0cac103639a5
Jenya <jeskov95@gmail.com>
2022-06-21 05:57:50 +0300

    add comments to the controlController.js

--- controllers/controlController.js
+++ controllers/controlController.js
@@ -1,6 +1,8 @@
 import controlService from "../models/controlService.js";
 
 export const controlController = {
+
+    //Starts tracking the time of reminders
     start: (req, res, next) => {
         try {
             let result = controlService.start();
@@ -12,8 +14,10 @@ export const controlController = {
         }
     },
 
-    stop: (req, res) => {
-        controlService.stop();
+    //Stops tracking the time of reminders
+    stop: (req, res, next) => {
+        try {
+            let result = controlService.stop();
 
             console.log(result);
             res.send(result);
