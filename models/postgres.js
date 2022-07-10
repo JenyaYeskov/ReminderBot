89963ec17b59c6b7a7da697bb7be4a34fd27eaed
Jenya <jeskov95@gmail.com>
2022-07-10 06:47:45 +0300

    add postgres.js

---
+++ models/postgres.js
@@ -0,0 +1,35 @@
+// import "dotenv/config";
+import pg from "pg";
+import "dotenv/config"
+
+const clientClass = pg.Client;
+
+// const client = new clientClass("postgres://gmlatebk:wlTVetyD86nERrzISLsDEFE9cSAVio0Q@abul.db.elephantsql.com/gmlatebk");
+
+
+class postgres {
+
+    async find(data) {
+    }
+
+    async createNew(data) {
+    }
+
+    async findOneAndDelete(data) {
+
+    }
+
+    async deleteMany(data) {
+
+    }
+
+    async getAll() {
+
+    }
+
+    async findByIdAndUpdate(id, data) {
+
+    }
+}
+
+export default new postgres();
