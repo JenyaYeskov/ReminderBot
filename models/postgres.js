6cd150a94946effeb6b32371b5e960f03ea7622c
Jenya <jeskov95@gmail.com>
2022-07-16 07:28:57 +0300

    refactor postgres.createNew() method to use a separate doRequest() method for db requests

--- models/postgres.js
+++ models/postgres.js
@@ -37,25 +37,15 @@ class postgres {
     }
 
     async createNew(data) {
-        let connection = await getConnection();
-        let {dateInput, timeInput, event, time, userReminderId} = data;
-        let uid = data["messenger user id"];
+        const {dateInput, timeInput, event, time, userReminderId, "messenger user id": uid} = data;
+        const queryData = [dateInput, timeInput, event, time, userReminderId, uid];
 
-        try {
-            await connection.connect();
+        const queryString = `INSERT INTO reminders ("dateInput", "timeInput", event, time, "userReminderId", "messenger user id") 
+                 VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
 
-            let result = await connection.query(
-                `INSERT INTO reminders ("dateInput", "timeInput", event, time, "userReminderId", "messenger user id") 
-                 VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`,
-                [dateInput, timeInput, event, time, userReminderId, uid]);
+        const result = await this.doRequest(queryString, queryData);
 
-            return result.rows[0];
-
-        } catch (e) {
-            throw e;
-        } finally {
-            await connection.end();
-        }
+        return result.rows[0];
     }
 
     async findOneAndDelete(data) {
